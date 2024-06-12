596f3d6a2eba3ae711da10696fd51675f28ba0ee
sunil batesar <sunilkhokha015@gmail.com>
2024-06-12 15:29:07 +0530

    creating login fun

---
+++ lib/Network/networkApiService.dart
@@ -0,0 +1,16 @@
+import 'package:chat_application/Network/BaseApiService.dart';
+
+class NetworkApiServices extends BaseApiServices {
+  @override
+  Future Signupfun(data) {
+    // TODO: implement Signupfun
+    throw UnimplementedError();
+  }
+
+  @override
+  Future loginfun(data) {
+    // TODO: implement loginfun
+    throw UnimplementedError();
+  }
+  
+}
